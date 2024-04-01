9bd02e2024d943b12f97333d778460e1d641a18a
Danil <danil.polovynkin.04@gmail.com>
2024-04-01 15:54:28 +0300

    added row implementation of cluster

--- src/components/GoogleMap/GoogleMap.tsx
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -1,16 +1,26 @@
-import { AdvancedMarker, Map, MapMouseEvent, Pin } from '@vis.gl/react-google-maps';
-import React, { useEffect, useState } from 'react';
+import { AdvancedMarker, Map, MapMouseEvent, Pin, useMap } from '@vis.gl/react-google-maps';
+import React, { useEffect, useRef, useState } from 'react';
 import { googleMapId } from '@/services/services';
+import { Marker, MarkerClusterer } from '@googlemaps/markerclusterer';
 
 const GoogleMap = () => {
     const cityPosition = { lat: 53.54, lng: 10 };
 
-    const [markerPosition, setMarkerPosition] = useState<{ lat: number; lng: number }>({ lat: 53.54, lng: 10 });
+    const map = useMap();
+    const [markersPosition, setMarkersPosition] = useState<{ lat: number; lng: number }[]>([]);
+    const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
 
-    const [markers, setMarkers] = useState<{ lat: number; lng: number }[]>([]);
+    const clusterRef = useRef<MarkerClusterer | null>(null);
+
+    useEffect(() => {
+        if (!map) return;
+        if (!clusterRef.current) {
+            clusterRef.current = new MarkerClusterer({ map });
+        }
+    }, [map]);
 
     const onMapClickHandler = (e: MapMouseEvent) => {
-        setMarkers((current) => [
+        setMarkersPosition((current) => [
             ...current,
             {
                 lat: e.detail.latLng?.lat ? e.detail.latLng?.lat : 0,
@@ -20,26 +30,48 @@ const GoogleMap = () => {
     };
 
     const onDragEndHandler = (e: google.maps.MapMouseEvent, index: number) => {
-        const updatedMarkers = [...markers];
+        const updatedMarkers = [...markersPosition];
 
         updatedMarkers[index] = {
             lat: e.latLng?.lat() ? e.latLng?.lat() : 0,
             lng: e.latLng?.lng() ? e.latLng?.lng() : 0,
         };
 
-        setMarkers(updatedMarkers);
+        setMarkersPosition(updatedMarkers);
     };
 
     const onRemoveButtonClickHandler = () => {
         setMarkers([]);
     };
 
+    useEffect(() => {
+        clusterRef.current?.clearMarkers();
+        clusterRef.current?.addMarkers(Object.values(markers));
+        console.log(markers);
+    }, [markers]);
+
+    const setMarkerRef = (marker: Marker | null, key: string) => {
+        if (marker && markers[key]) return;
+        if (!marker && !markers[key]) return;
+
+        setMarkers((prev) => {
+            if (marker) {
+                return { ...prev, [key]: marker };
+            } else {
+                const newMarkers = { ...prev };
+                delete newMarkers[key];
+                return newMarkers;
+            }
+        });
+    };
+
     return (
-        <div style={{ height: '100vh', width: '100%', position: 'relative', display: 'block' }}>
-            <Map zoom={9} center={cityPosition} mapId={googleMapId} onClick={(e: MapMouseEvent) => onMapClickHandler(e)}>
-                {markers.map((marker, index) => {
+        <div className='map-container'>
+            <Map defaultZoom={9} center={cityPosition} mapId={googleMapId} onClick={(e: MapMouseEvent) => onMapClickHandler(e)}>
+                {markersPosition.map((marker, index) => {
                     return (
                         <AdvancedMarker
+                            ref={(marker) => setMarkerRef(marker, index.toString())}
                             draggable
                             onDragEnd={(e) => {
                                 onDragEndHandler(e, index);
@@ -47,7 +79,7 @@ const GoogleMap = () => {
                             key={index}
                             position={{ lat: marker.lat, lng: marker.lng }}
                             onClick={() => {
-                                setMarkers((current) => {
+                                setMarkersPosition((current) => {
                                     return current.filter((el, elIndex) => elIndex !== index);
                                 });
                             }}>
@@ -59,12 +91,6 @@ const GoogleMap = () => {
                         </AdvancedMarker>
                     );
                 })}
-                {/* 
-                {open && (
-                    <InfoWindow position={cityPosition}>
-                        <p>number of pin</p>
-                    </InfoWindow>
-                )} */}
             </Map>
             <button className='button' onClick={onRemoveButtonClickHandler}>
                 Remove all the marks
